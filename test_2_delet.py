3a83201e08bf2260a3629bdbd9b1058de40232b3
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-04-07 18:00:31 +0300

    Update test_2_delet.py

--- test_2_delet.py
+++ test_2_delet.py
@@ -1,3 +1,8 @@
-from requests import post, delete
+from requests import get, post, delete
 
-print(delete('http://localhost:5000/api/jobs/4'))
+print(get('http://localhost:5000/api/jobs'))
+print(delete('http://localhost:5000/api/jobs/1'))
+print(get('http://localhost:5000/api/jobs'))
+
+print(delete('http://localhost:5000/api/jobs/re'))
+print(delete('http://localhost:5000/api/jobs/20'))
