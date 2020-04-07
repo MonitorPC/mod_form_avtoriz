578ad9bb86a3fee7f19e1ce513ee4eaf20d4db18
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-04-07 16:37:22 +0300

    Add files via upload

---
+++ test_2_post.py
@@ -0,0 +1,12 @@
+from requests import post
+
+print(post('http://localhost:5000/api/news').json())
+print(post('http://localhost:5000/api/news',
+           json={'title': 'Заголовок'}).json())
+print(post('http://localhost:5000/api/jobs', json={
+    'id': 1,
+    'team_leader': 3,
+    'job': 'РАБОТА РАБОТА НИЧЕГО КРОМЕ РАБОТЫ',
+    'work_size': 3,
+    'collaborators': '1, 2',
+    'is_finished': True}).json())
