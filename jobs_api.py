70f010e0e1886902841a046f6588fd88103d87c4
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-04-07 18:30:28 +0300

    Update jobs_api.py

--- jobs_api.py
+++ jobs_api.py
@@ -67,3 +67,26 @@ def delete_jobs(jobs_id):
     session.delete(jobs)
     session.commit()
     return jsonify({'success': 'OK'})
+
+  
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['POST'])
+def edit_jobs(jobs_id):
+    session = db_session.create_session()
+
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size', 'collaborators', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+    elif not session.query(Jobs).filter(Jobs.id == jobs_id).first():
+        return jsonify({'error': 'Id not founded'})
+
+    jobs = session.query(Jobs).filter(Jobs.id == jobs_id).first()
+    jobs.team_leader = request.json['team_leader']
+    jobs.job = request.json['job']
+    jobs.work_size = request.json['work_size']
+    jobs.collaborators = request.json['collaborators']
+    jobs.is_finished = request.json['is_finished']
+
+    session.commit()
+    return jsonify({'success': 'OK'})
